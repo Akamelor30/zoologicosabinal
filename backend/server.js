ea4c1a373a010bb1017995a6ca5686de0e79b66a
Akamelor30 <ramirezerickdamian30@gmail.com>
2026-04-14 01:07:30 -0600

    Quitar valores por defecto de MySQL en server.js

--- backend/server.js
+++ backend/server.js
@@ -64,15 +64,26 @@ if (fs.existsSync(FRONTEND_DIR)) {
 // ============================================
 // 🗄️ CONEXIÓN A MySQL
 // ============================================
+//const pool = mysql.createPool({
+//    host: process.env.DB_HOST || 'localhost',
+//    user: process.env.DB_USER || 'root',
+//    password: process.env.DB_PASSWORD || '123',
+//    database: process.env.DB_NAME || 'zoologicosabinal',
+ //   waitForConnections: true,
+ //   connectionLimit: 10,
+//    queueLimit: 0
+//});
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '123',
-    database: process.env.DB_NAME || 'zoologicosabinal',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 // ============================================
 // 📧 CONFIGURACIÓN DE CORREO
